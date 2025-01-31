2176e80a4f15b8103df2edfd157067a7d242c3fe
tzheng-7 <tiantianz258@deltalearns.ca>
2025-01-31 14:45:34 -0800

    Update main.py

--- main.py
+++ main.py
@@ -8,3 +8,21 @@
 # overspent or underspent or spent just the right amount, write
 # a specific message - try to be kind!)
 
+# Create variables
+
+# Create list
+expenses_list = []
+
+# Create function
+total = 0
+total = sum(expenses_list)
+print(f"Your total spending this month was ${total}!")
+
+# Introdction
+loop
+input(f"Hi! Thank you for using this finance tracker. How many
+      expenses would you like to calculate today? ")
+
+
+
+# Ask for # of expenses
