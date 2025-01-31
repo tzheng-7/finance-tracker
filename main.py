075ef0d8fbdf054523081e3a48fd92f8884b2485
tzheng-7 <tiantianz258@deltalearns.ca>
2025-01-31 13:56:09 -0800

    Added basic commnenting
    This is the start in creating a finance tracker that will ask the user for monthly allowance and calculate total expenses and remaining balance.

---
+++ main.py
@@ -0,0 +1,10 @@
+# Ask for monthly allowance and expenses in different categories
+
+# Calculate total expenses and remaining balance
+
+# Display results
+
+# Conditional message based on balance (depending on whether you
+# overspent or underspent or spent just the right amount, write
+# a specific message - try to be kind!)
+
